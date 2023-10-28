ae8a8364ff3048231e55bc20127b2b81986475e3
Hévila Santos <hevilaos@gmail.com>
2023-10-28 19:38:11 -0300

    Starting

---
+++ src/BankInterface.java
@@ -0,0 +1,11 @@
+public class BankInterface {
+
+    public void showBankMenu()
+    {
+        String menu = """
+                ********************************
+                Dados iniciais do cliente
+                """;
+        System.out.println(menu);
+    }
+}
---
+++ src/ClientBankAccount.java
@@ -0,0 +1,30 @@
+public class ClientBankAccount {
+    private String clientName;
+    private String accountType;
+    private double accountBalance;
+
+    //Constructor will set the variables because the project does not require to receive
+    //other data (except account balance)
+    public ClientBankAccount(){
+        clientName = "Hévila de Oliveira Santos";
+        accountType = "Corrente";
+        accountBalance = 2500;
+    }
+
+    //getters and setters
+    public String getClientName(){
+        return clientName;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+}
