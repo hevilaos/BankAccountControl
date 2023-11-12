ca0d95216585df5d1e3daa6107997c55e30bad27
Hevila <hevila.santos@trustly.com>
2023-11-12 17:07:38 -0300

    Mostrando Menu Inicial

--- src/BankInterface.java
+++ src/BankInterface.java
@@ -1,11 +1,23 @@
 public class BankInterface {
 
-    public void showBankMenu()
+    public void showBankMenu(double saldo, String cliente, String tipoConta)
     {
         String menu = """
                 ********************************
                 Dados iniciais do cliente
-                """;
+                
+                Nome: %s
+                Tipo de conta: %s
+                Saldo inicial: R$ %.2f
+                
+                ********************************
+                
+                Operações
+                1 - Consultar saldos
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                """.formatted(cliente, tipoConta, saldo);
         System.out.println(menu);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        ClientBankAccount clientBankAccount = new ClientBankAccount();
+        double saldo = clientBankAccount.getAccountBalance();
+        String clienteNome = clientBankAccount.getClientName();
+        String clienteConta = clientBankAccount.getAccountType();
+
         BankInterface bankInterface = new BankInterface();
 
-        bankInterface.showBankMenu();
+        bankInterface.showBankMenu(saldo, clienteNome, clienteConta);
     }
 }
